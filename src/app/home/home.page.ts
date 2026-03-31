86c1417c5bdffc54e832d9f25cd77ea689594bbc
braslamptey <braslamptey18@gmail.com>
2026-03-31 13:09:17 +0100

    Correção do erro de pipe

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // ADICIONE ESTA LINHA
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone: true, // Certifique-se que isso está aqui
+  imports: [IonicModule, CommonModule, FormsModule], // ADICIONE O CommonModule AQUI
 })
 export class HomePage {
   valorConta: number = 0;
