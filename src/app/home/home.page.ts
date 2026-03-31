0f6434245beb870e02aaaf4233c97b08c538e822
braslamptey <braslamptey18@gmail.com>
2026-03-31 18:54:54 +0100

    Boa melhoria

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // ADICIONE ESTA LINHA
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 
@@ -7,18 +7,35 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true, // Certifique-se que isso está aqui
-  imports: [IonicModule, CommonModule, FormsModule], // ADICIONE O CommonModule AQUI
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class HomePage {
-  valorConta: number = 0;
+  valorConta: number | null = null;
   porcentagem: number = 10;
+  numPessoas: number = 1;
+  
   total: number = 0;
+  totalPorPessoa: number = 0;
 
   constructor() {}
 
+  selecionarPorcentagem(valor: number) {
+    this.porcentagem = valor;
+    this.calcular();
+  }
+
   calcular() {
-    let gorjeta = this.valorConta * (this.porcentagem / 100);
-    this.total = Number(this.valorConta) + Number(gorjeta);
+    const valor = Number(this.valorConta);
+    if (valor > 0) {
+      let gorjeta = valor * (this.porcentagem / 100);
+      this.total = valor + gorjeta;
+      
+      let pessoas = this.numPessoas > 0 ? this.numPessoas : 1;
+      this.totalPorPessoa = this.total / pessoas;
+    } else {
+      this.total = 0;
+      this.totalPorPessoa = 0;
+    }
   }
 }
